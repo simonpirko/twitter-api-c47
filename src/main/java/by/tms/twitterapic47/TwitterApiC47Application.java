39d1c307269d29e60beaaf9ba81a4bba223c352d
KLISHEVSKIY\Kliis <i.klishevskiy@mail.ru>
2022-01-18 19:29:42 +0300

    init commit.

---
+++ src/main/java/by/tms/twitterapic47/TwitterApiC47Application.java
@@ -0,0 +1,13 @@
+package by.tms.twitterapic47;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiC47Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterApiC47Application.class, args);
+    }
+
+}
