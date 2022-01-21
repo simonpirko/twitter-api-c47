04cda72cabe6a82e1fee54a08f4e551988103e83
TorusTredent <torus.tredent@gmail.com>
2022-01-21 23:27:20 +0300

    rebase CommentController, CommentService, add preCreate localDate for Comment, add UpdateCommentDto

--- src/main/java/by/tms/twitterapic47/controller/CommentController.java
+++ src/main/java/by/tms/twitterapic47/controller/CommentController.java
@@ -1,8 +1,19 @@
 package by.tms.twitterapic47.controller;
 
+import by.tms.twitterapic47.dto.comment.ResponseCommentDto;
+import by.tms.twitterapic47.dto.comment.SaveCommentDto;
+import by.tms.twitterapic47.dto.comment.UpdateCommentDto;
+import by.tms.twitterapic47.dto.post.ResponsePostDto;
+import by.tms.twitterapic47.entity.Comment;
 import by.tms.twitterapic47.service.CommentService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/comment")
@@ -11,27 +22,33 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
-//    @PostMapping("/add/{postId}")
-//    public ResponseEntity<?> add(@PathVariable("postId") long postId, @RequestBody SaveCommentDto saveCommentDto) {
-//        Comment add = commentService.add(convertSaveCommentDtoToComment(saveCommentDto), postId, saveCommentDto.getUsername());
-//        GetSaveCommentDto getSaveComment = convertCommentToGetSaveCommentDto(add);
-//        return new ResponseEntity<>(getSaveComment, HttpStatus.OK);
-//    }
-//
-//
-//    private Comment convertSaveCommentDtoToComment(SaveCommentDto saveCommentDto) {
-//        Comment comment = new Comment();
-//        comment.setDescription(saveCommentDto.getDescription());
-//        return comment;
-//    }
-//
-//    private GetSaveCommentDto convertCommentToGetSaveCommentDto(Comment comment) {
-//        GetSaveCommentDto getSaveComment = new GetSaveCommentDto();
-//        getSaveComment.setId(comment.getId());
-//        getSaveComment.setCreatorName(comment.getCreator().getUsername());
-//        getSaveComment.setDescription(comment.getDescription());
-//        getSaveComment.setDateCreating(comment.getDateCreating());
-//        getSaveComment.setPostName(comment.getPost().getTitle());
-//        return getSaveComment;
-//    }
+    @Autowired
+    private ModelMapper mapper;
+
+    @PostMapping("/add/{postId}")
+    public ResponseEntity<?> add(@PathVariable long postId, @RequestBody SaveCommentDto saveCommentDto) {
+        Comment add = commentService.add(mapper.map(saveCommentDto, Comment.class), postId);
+        return new ResponseEntity<>(mapper.map(add, ResponseCommentDto.class), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<?> delete(@PathVariable long commentId) {
+        Comment delete = commentService.delete(commentId);
+        return new ResponseEntity<>(mapper.map(delete, ResponseCommentDto.class), HttpStatus.OK);
+    }
+
+    @GetMapping("/{postId}/getAll")
+    public ResponseEntity<?> getCommentsByPostId(@PathVariable long postId) {
+        List<ResponseCommentDto> commentDto = commentService.getCommentsByPostId(postId)
+                .stream()
+                .map(comment -> mapper.map(comment, ResponseCommentDto.class))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(commentDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/edit/{commentId}")
+    public ResponseEntity<?> edit(@PathVariable long commentId, @RequestBody UpdateCommentDto updateComment) {
+        Comment edit = commentService.edit(commentId, updateComment.getDescription());
+        return new ResponseEntity<>(mapper.map(edit, ResponseCommentDto.class), HttpStatus.OK);
+    }
 }
--- src/main/java/by/tms/twitterapic47/service/CommentService.java
+++ src/main/java/by/tms/twitterapic47/service/CommentService.java
@@ -26,12 +26,10 @@ public class CommentService {
     @Autowired
     private UserRepository userRepository;
 
-    public Comment add(Comment comment, long postId, String username) {
-        User byUsername = userRepository.findByUsername(username).orElseThrow(()->new RuntimeException("User not found"));
+    public Comment add(Comment comment, long postId) {
+        userRepository.findByUsername(comment.getCreatorUsername()).orElseThrow(()->new RuntimeException("User not found"));
         Post currentPost = postRepository.findById(postId).orElseThrow(()->new RuntimeException("Post not found"));
-        comment.setCreatorUsername(byUsername.getUsername());
         comment.setPostId(currentPost.getId());
-        comment.setDateCreating(setDate());
         return commentRepository.save(comment);
     }
 
@@ -41,12 +39,8 @@ public class CommentService {
         return commentById;
     }
 
-    public Comment getCommentById(long id){
-        return commentRepository.findById(id).orElseThrow(()->new RuntimeException("Comments not found"));
-    }
-
-    public List<Comment> getAllCommentsByPostId(long postId) {
-        Post postById = postRepository.findById(postId).orElseThrow(()->new RuntimeException("Post not found"));
+    public List<Comment> getCommentsByPostId(long postId) {
+        postRepository.findById(postId).orElseThrow(()->new RuntimeException("Post not found"));
         List<Comment> allByPostId = commentRepository.findAllByPostId(postId);
         if(allByPostId.isEmpty()){
             throw new RuntimeException("Comments not found");
@@ -55,11 +49,10 @@ public class CommentService {
         }
     }
 
-    private String setDate() {
-        Date date = new Date();
-        SimpleDateFormat format1 = new SimpleDateFormat(
-                "dd.MM.yyyy hh:mm");
-        return format1.format(date);
+    public Comment edit(long commentId, String description) {
+        Comment commentById = commentRepository.findById(commentId).orElseThrow(() -> new RuntimeException("Comment not found"));
+        commentById.setDescription(description);
+        commentRepository.save(commentById);
+        return commentById;
     }
-
 }
