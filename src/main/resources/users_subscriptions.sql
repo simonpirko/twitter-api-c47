58cdc4e9142d74f79807f679983191cf098ca28d
KLISHEVSKIY\Kliis <i.klishevskiy@mail.ru>
2022-01-22 20:25:57 +0300

    example sql for table user subscription.

---
+++ src/main/resources/users_subscriptions.sql
@@ -0,0 +1,3 @@
+insert into user_subscriptions (user_id, subscriptions) values ('1', 'dima')
+insert into user_subscriptions (user_id, subscriptions) values ('1', 'alex')
+insert into user_subscriptions (user_id, subscriptions) values ('1', 'maxim')
