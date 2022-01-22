ff99337f402fd0ab37d5ce1991c9d0261553ed2b
artemyaroshevich <artem.yaroshevich.2205@gmail.com>
2022-01-22 18:46:25 +0300

    initialization data

--- src/main/resources/post_init.sql
+++ src/main/resources/post_init.sql
@@ -13,3 +13,35 @@ insert into post (id, creator_username, date_creating, description, title) value
 insert into post (id, creator_username, date_creating, description, title) values (null, 'semen', '18.01.2022 01:21', 'semen description 3', 'semen comment');
 insert into post (id, creator_username, date_creating, description, title) values (null, 'igor', '17.01.2022 01:21', 'igor description 3', 'igor comment');
 insert into post (id, creator_username, date_creating, description, title) values (null, 'andrey', '15.01.2022 01:21', 'andrey description 3', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'gleb', '15.01.2022 01:25', 'gleb description 3', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'andrey', '15.01.2022 01:21', 'andrey description 3', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'gleb', '15.01.2022 01:25', 'gleb description 3', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'arnold21', '14.01.2022 01:23', 'arnold21 description 5', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'arnold21', '16.01.2022 01:25', 'arnold21 description 3', 'denis78 comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'toly17', '19.01.2022 01:25', 'toly17 description 3', 'denis78 comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'toly17', '18.01.2022 01:28', 'toly17 description 6', 'georgi comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'arturik', '16.01.2022 01:29', 'arturik description 5', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'arturik', '19.01.2022 01:25', 'arturik description 3', 'vadim123 comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'borisbritva', '19.01.2022 01:25', 'borisbritva description 4', 'cannibal comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'borisbritva', '03.01.2022 01:25', 'borisbritva description 6', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'valik91', '02.01.2022 01:27', 'valik91 description 6', 'vasya45 comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'valik91', '03.01.2022 01:26', 'valik91 description 6', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'vasya45', '04.01.2022 01:26', 'vasya45 description 6', 'gektor comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'vasya45', '05.01.2022 01:26', 'vasya45 description 6', 'gektor comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'gektor', '07.01.2022 01:26', 'gektor description 6', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'gektor', '06.01.2022 01:26', 'gektor description 6', 'cannibal comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'cannibal', '10.01.2022 01:26', 'cannibal description 4', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'cannibal', '11.01.2022 01:26', 'cannibal description 11', 'vasya45 comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'georgi', '12.01.2022 01:26', 'georgi description 11', 'andrey comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'georgi', '13.01.2022 01:26', 'georgi description 14', 'cannibal comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'denis78', '14.01.2022 01:25', 'georgi description 14', 'vasya45 comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'denis78', '15.01.2022 01:16', 'georgi description 15', 'cannibal comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'egor', '17.01.2022 01:16', 'egor description 1', 'cannibal comment');
+insert into post (id, creator_username, date_creating, description, title) values (null, 'egor', '16.01.2022 01:16', 'egor description 16', 'cannibal comment');
+
+
+
+
+
+
+
