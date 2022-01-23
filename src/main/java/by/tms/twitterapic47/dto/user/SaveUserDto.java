6a88c80d345866c172e6308290fb8b74487fa55d
TorusTredent <torus.tredent@gmail.com>
2022-01-23 13:10:23 +0300

    add validation for SaveUserDto

--- src/main/java/by/tms/twitterapic47/dto/user/SaveUserDto.java
+++ src/main/java/by/tms/twitterapic47/dto/user/SaveUserDto.java
@@ -3,7 +3,10 @@ package by.tms.twitterapic47.dto.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
@@ -11,13 +14,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class SaveUserDto {
 
+    @NotBlank
+    @NotEmpty
+    @Length(max = 255)
     private String username;
 
+    @NotBlank
+    @NotEmpty
+    @Length(min = 3, max = 255)
     private String password;
 
+    @NotBlank
+    @NotEmpty
+    @Length(min = 5, max = 255)
+    @Email
     private String email;
 
+    @NotBlank
+    @NotEmpty
+    @Length(max = 255)
     private String firstName;
 
+    @NotBlank
+    @NotEmpty
+    @Length(max = 255)
     private String lastName;
 }
